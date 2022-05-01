d39a9faa40ed183a3d44c5cca1e50d2d387e7139
Zdanovich1 <zdanovich.fti@gmail.com>
2022-05-01 12:52:15 +0300

    Common character count

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+
+  let newArr = arr.slice();
+
+  // console.log(arr);
+  //console.log(newArr);
+
+  let resultArr = [];
+
+  for (let i = 0; i < newArr.length; i++) {
+    /*basic sequence interactions work well*/
+    if (typeof newArr[0] == 'string') {
+      newArr.shift()
+    }
+    if (typeof newArr[newArr.length - 1] == 'string') {
+      newArr.pop()
+    }
+
+
+
+    if (newArr[i] === '--discard-next') {
+      //console.log(newArr[i])
+      newArr.splice(i, 2)
+      //console.log(newArr[i])
+    }
+
+    if (newArr[i] === '--discard-prev') {
+      newArr.splice(i - 1, 2)
+    }
+
+    if (newArr[i] === '--double-next') {
+
+      newArr.splice(i, 1, newArr[i + 1])
+    }
+
+    if (newArr[i] === '--double-prev') {
+      // console.log('catch')
+      // console.log(newArr[i])
+      newArr.splice(i - 1, 0, newArr[i - 1])
+      // console.log(newArr[i])
+      newArr.splice(i + 1, 1)
+
+
+    }
+
+
+  }
+
+  // console.log(arr);
+  return newArr;
+
 }
 
 module.exports = {
