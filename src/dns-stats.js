475308fd1abc6236e6e0eaae6b5363ea42fc1d82
Pavel Zdanovich <zdanovich.fti@gmail.com>
2022-07-26 17:39:22 +0300

    solved dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+
+  let arr = [];
+
+  domains.map(function (elem) {
+    let firstDotIndex = elem.indexOf('\.');
+    let secondDotIndex = elem.lastIndexOf('\.')
+
+    if (firstDotIndex !== secondDotIndex) {
+
+      arr.push('\.' + elem)
+      arr.push(elem.slice(firstDotIndex, elem.length))
+      arr.push(elem.slice(secondDotIndex, elem.length))
+
+    } else {
+
+      arr.push('\.' + elem)
+      arr.push(elem.slice(firstDotIndex, elem.length))
+
+    }
+  })
+
+  let arr1 = arr.map(item => '\.' + item.split('.').reverse().join('.')).map(el => el.slice(0, el.length - 1))
+  arr1.sort((a, b) => a.length - b.length)
+
+  let result = {};
+  let count = 1;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr1[i + 1]) {
+      count = count + 1;
+    } else if (arr1[i] !== arr1[i + 1]) {
+      result[arr1[i]] = count;
+      arr1.splice(0, i);
+      i = 0;
+      count = 1;
+    }
+  }
+  return result
 }
 
 module.exports = {
